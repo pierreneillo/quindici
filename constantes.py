8470b2eac2671785c3fb923139ed6f7b4e84cf28
pierreneillo <75447740+pierreneillo@users.noreply.github.com>
2022-04-08 09:07:53 +0200

    Added __about__ and cleaned comments

--- constantes.py
+++ constantes.py
@@ -1,30 +1,50 @@
-
-# ---
-# --- CONSTANTES POUR LE MODELE
+__about__ = """Module constantes.py: constantes pour le jeu de scopa
+----------------------------------------------------------------------------
+| Variable            | Utilisation/Signification                          |
+|__________________________________________________________________________|
+|                     |                                                    |
+| COULEURS            | Les quatres couleurs du jeu de cartes:             |
+|                     | clubs, diamonds, hearts et spades                  |
+----------------------------------------------------------------------------
+| HAUTEURS_SCOPA      | Les valeurs de cartes du jeu de scopa              |
+----------------------------------------------------------------------------
+| DICO_COULEURS       | Associe chaque couleur à son indice dans COULEURS  |
+----------------------------------------------------------------------------
+| DICO_HAUTEURS_SCOPA | Associe chaque hauteur à son indice dans HAUTEURS  |
+----------------------------------------------------------------------------
+| DICO_VALEURS        | Associe chaque hauteur à sa valeur dans le jeu     |
+----------------------------------------------------------------------------
+|                     | Positions des joueurs en fonction du nombre        |
+| POSITIONS           |   - 2 joueurs : Sud et Nord                        |
+|                     |   - 3 joueurs : Sud, Est et Nord                   |
+|                     |   - 4 joueurs : Sud, Est, Nord et Ouest            |
+----------------------------------------------------------------------------
+| VALUE_ERROR         | L'erreur devant être renvoyée lorsqu'il y a une    |
+|                     | incohérence dans la construction du paquet         |
+----------------------------------------------------------------------------
+|                          Attributs graphiques                            |
+----------------------------------------------------------------------------
+| DISTANCE            | Distance au centre du tapis en pixels              |
+----------------------------------------------------------------------------
+| CARTE_H, CARTE_L    | Dimensions des images des cartes                   |
+----------------------------------------------------------------------------
+| X_TAPIS, Y_TAPIS    | Coordonnées du tapis                               |
+----------------------------------------------------------------------------
+| X_TALON, Y_TALON    | Coordonnées du talon                               |
+----------------------------------------------------------------------------
+| REPERES             | Repères pour la position des mains des joueurs     |
+----------------------------------------------------------------------------
+"""
 
 COULEURS = ['c', 'd', 'h', 's']
-HAUTEURS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 HAUTEURS_SCOPA = ['1', '2', '3', '4', '5', '6', '7', 'J', 'Q', 'K']
-#couleurs : c, d, h, s correspondant à 0, 1, 2, 3
-#clubs, diamonds , hearts and spades.
-#hauteurs : 1 pour l'As jusqu'à 10 pour le 10, puis J, Q, K
-#J pour Jack (Valet), Q pour Queen et K pour King
-#correspondant aux indices de 0 à 12 (As au Roi)
 
 
 DICO_COULEURS = {'c':0, 'd':1, 'h':2, 's':3}
-DICO_HAUTEURS = {'1':0, '2':1, '3':2, '4':3, '5':4,
-    '6':5, '7':6, '8':7, '9':8, '10':9, 'J':10, 'Q':11, 'K':12}
+DICO_HAUTEURS_SCOPA = {'1':0, '2':1, '3':2, '4':3, '5':4,
+    '6':5, '7':6, 'J':7, 'Q':8, 'K':9}
 DICO_VALEURS = {'1':1, '2':2, '3':3, '4':4, '5':5,
     '6':6, '7':7, '8':None, '9':None, '10':None, 'J':8, 'Q':9, 'K':10}
-#au pharaon, chaque carte représente un nombre de points égale à sa valeur faciale (1 pour l'As, 2 pour 2...)
-#sauf les honneurs qui valent chacun 10 points
-#DICO_VALEURS = ....
-
-# Positions des joueurs en fonction du nombre
-# 2 joueurs : Sud et Nord
-# 3 joueurs : Sud, Est et Nord
-# 4 joueurs : Sud, Est, Nord et Ouest
 
 POSITIONS = '', 'S', 'SN', 'SEN', 'SENO'
 
@@ -32,21 +52,12 @@ POSITIONS = '', 'S', 'SN', 'SEN', 'SENO'
 
 VALUE_ERROR = 'nb_joueurs doit être entre 2 et 4, nb_cartes est égal à 32 ou à 52, ou à 40'
 
-
-# ---
-# --- CONSTANTES POUR LA VUE TKINTER
-
-# distance au centre du tapis en pixel
 DISTANCE = 400
 
-
-
-#dimensions des images des cartes
 CARTE_H = 96
 CARTE_L = 71
 
-#TAPIS et TALON
-X_TAPIS, Y_TAPIS = 425, 350
+X_TAPIS, Y_TAPIS = 425,350
 X_TALON, Y_TALON = 350,350
 
 
